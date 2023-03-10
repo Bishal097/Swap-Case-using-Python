5ff731107cde25171b4a43ab334f874c68ca3a19
Bishal Bhattacharjee <103323091+Bishal097@users.noreply.github.com>
2023-03-10 23:45:32 +0530

    Add files via upload

---
+++ bishal python.py
@@ -0,0 +1,9 @@
+result = []
+    for letter in s:
+        if letter == letter.lower():
+            result.append(letter.upper())
+        elif letter == letter.upper():
+            result.append(letter.lower())
+        else:
+            result.append(letter)
+    return ''.join(result)
